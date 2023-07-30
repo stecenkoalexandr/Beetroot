1490743b05cd4b26bad996c6749bb2dc4ff4281a
Oleksandr Stetsenko <stecenkoalexandr@gmail.com>
2023-07-30 22:03:06 +0300

    my second commit

---
+++ home_work/home_4.py
@@ -0,0 +1,61 @@
+# Task 1
+var = str(input("Input anything, please!\n"))
+inp = "You input:( " + var + ") symbols"
+print(inp)
+if len(var) < 2:
+    print("You input not enought symbols, try again please")
+else:
+    f_1 = var[0:2]
+    f_2 = var[-2:]
+    print(f_1 + f_2)
+# Task 2
+
+get_num = input("Please input phone number: \n")
+
+if get_num.isdigit():  # Check if input consists of digits only
+    print("You input: " + get_num + " number\n")
+
+    if len(get_num) == 10:
+        print("It's a valid phone number.\n")
+    else:
+        print("It's not a valid phone number.")
+else:
+    print("You did not input a number.")
+
+# Task 3
+import random
+
+num1 = random.randint(0, 99)
+num2 = random.randint(0, 99)
+symbol = random.choice(['+', '-', '*', '/'])
+
+print("Welcome Mathematick battle")
+print(str(num1) + symbol + str(num2) + " = ?\n")
+
+user_input = input("Please input your answer here: \n")
+user_answer = int(user_input)  # Convert the user's input to an integer
+
+if symbol == '+':
+    correct_answer = num1 + num2
+elif symbol == '-':
+    correct_answer = num1 - num2
+elif symbol == '*':
+    correct_answer = num1 * num2
+elif symbol == '/':
+    correct_answer = num1 / num2  # Note: This is a floating-point division
+
+if user_answer == correct_answer:
+    print("Your input is the correct answer:", user_answer)
+else:
+    print("Sorry, you made a mistake. The correct answer is:", correct_answer)
+
+# Task 4
+
+my_name = "Oleksandr"
+
+user_input = input("Please enter your name: ")
+
+if user_input.lower() == my_name:
+    print(" It`s True")
+else:
+    print("It`s False")
---
+++ home_work/home_5.py
@@ -0,0 +1,26 @@
+# Task 1
+import random
+
+secret_number = random.randint(1, 10)
+user_guess = int(input("Guess the number (between 1 and 10): "))
+
+if user_guess == secret_number:
+    print("Congratulations! Your guess is correct.")
+else:
+    print(f"Sorry, the correct number was {secret_number}.")
+
+# Task 2
+name = input("Please enter your name: ")
+age = int(input("Please enter your age: "))
+next_birthday_age = age + 1
+print(f"Hello {name}, on your next birthday you'll be {next_birthday_age} years.")
+
+# Task 3
+import random
+
+input_string = input("Please enter an input string: ")
+random_strings = [''.join(random.sample(input_string, len(input_string))) for _ in range(5)]
+
+print("Random Strings:")
+for random_string in random_strings:
+    print(random_string)
